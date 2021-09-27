f4da17999ed64cca5c447edea9dde2d34456f1f1
risa <risa.mystudy@gmail.com>
2021-09-27 06:51:23 +0000

    resubmit1

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,8 +1,7 @@
-kojin_jouhou = {
-    myouji: '田中',
-    namae: '太郎',
-    nenrei: 25
-}
-kadai = kojin_jouhou[:myouji] + kojin_jouhou[:namae] + 'です。' + kojin_jouhou[:nenrei].to_s + '歳です。'
+myouji = '田中'
+namae = '太郎'
+nenrei = 25
+
+kadai = myouji + namae + 'です。' + nenrei.to_s + '歳です。'
 puts kadai
 
